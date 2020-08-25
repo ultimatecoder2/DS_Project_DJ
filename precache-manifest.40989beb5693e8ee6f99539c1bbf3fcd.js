7673937bf8a3b7e6d404f11188f854150bb49f80
Mickey_413 <43994883+ultimatecoder2@users.noreply.github.com>
2020-08-25 22:37:21 +0530

    Updates

---
+++ precache-manifest.40989beb5693e8ee6f99539c1bbf3fcd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda8a8b07d2f177889a53df03b50165c",
+    "url": "/DS_Project_DJ/index.html"
+  },
+  {
+    "revision": "6cd4c0fa2bcbc2250d5a",
+    "url": "/DS_Project_DJ/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "002c3deb26e570f4fad9",
+    "url": "/DS_Project_DJ/static/css/main.cf0bde5d.chunk.css"
+  },
+  {
+    "revision": "6cd4c0fa2bcbc2250d5a",
+    "url": "/DS_Project_DJ/static/js/2.f37bd114.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/DS_Project_DJ/static/js/2.f37bd114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002c3deb26e570f4fad9",
+    "url": "/DS_Project_DJ/static/js/main.41566a6b.chunk.js"
+  },
+  {
+    "revision": "36ff94a5be1a7b072fb5",
+    "url": "/DS_Project_DJ/static/js/runtime-main.e61e88a6.js"
+  },
+  {
+    "revision": "cd8dce0cfc60507814907f3b30902c8f",
+    "url": "/DS_Project_DJ/static/media/Lions.cd8dce0c.svg"
+  },
+  {
+    "revision": "bcdd3a2b09ed42969180f15d81464126",
+    "url": "/DS_Project_DJ/static/media/PecNew1.bcdd3a2b.png"
+  },
+  {
+    "revision": "ae2907837e447285a7d1c2c645c86c8c",
+    "url": "/DS_Project_DJ/static/media/User2.ae290783.png"
+  }
+]);
